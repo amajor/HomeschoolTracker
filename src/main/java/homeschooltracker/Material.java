c9d5b5d1eb153e2d106f9c4b42f1f8f43301d2d7
Alison Major <alison.major@gmail.com>
2020-09-26 16:16:37 -0400

    Create Material class and tests

---
+++ src/main/java/homeschooltracker/Material.java
@@ -0,0 +1,35 @@
+package homeschooltracker;
+
+public class Material {
+    String description;
+    Boolean graded;
+
+    public Material(
+            String description,
+            boolean graded)
+    {
+        this.description = description;
+        this.graded = graded;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Boolean isGraded() {
+        return graded;
+    }
+
+    public void setGraded(Boolean graded) {
+        this.graded = graded;
+    }
+
+    @Override
+    public String toString() {
+        String check = " ";
+        if (graded) {
+            check = "X";
+        }
+        return ("\n\n  [" + check + "] " + description);
+    }
+}
---
+++ src/test/java/homeschooltracker/MaterialTest.java
@@ -0,0 +1,44 @@
+package homeschooltracker;
+
+import junit.framework.TestCase;
+
+public class MaterialTest extends TestCase {
+    // Initialize test data
+    String description1 = "Worksheet A";
+    String description2 = "Phonics Book, Page 32";
+
+    Material material1 = new Material(
+        description1,
+        true
+    );
+
+    Material material2 = new Material(
+        description2,
+        false
+    );
+
+    public void testGetDescription() {
+        assertEquals(description1, material1.getDescription());
+        assertEquals(description2, material2.getDescription());
+    }
+
+    public void testIsGraded() {
+        assertTrue(material1.isGraded());
+        assertFalse(material2.isGraded());
+    }
+
+    public void testToString() {
+        String expected1 = "\n\n  [X] " + description1;
+        String expected2 = "\n\n  [ ] " + description2;
+        assertEquals(expected1, material1.toString());
+        assertEquals(expected2, material2.toString());
+    }
+
+    public void testSetGraded() {
+        material1.setGraded(false);
+        assertFalse(material1.isGraded());
+
+        material2.setGraded(true);
+        assertTrue(material2.isGraded());
+    }
+}
