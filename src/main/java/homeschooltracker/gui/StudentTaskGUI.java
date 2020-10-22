707dc6bcb370aa27ce18f45b60d13cb54c29e83f
Alison Major <alison.major@gmail.com>
2020-10-22 01:00:28 -0400

    Add buttons to student gui

--- src/main/java/homeschooltracker/gui/StudentTaskGUI.java
+++ src/main/java/homeschooltracker/gui/StudentTaskGUI.java
@@ -1,9 +1,14 @@
 package homeschooltracker.gui;
 
+import homeschooltracker.content.Task;
 import homeschooltracker.users.Student;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class StudentTaskGUI {
 
@@ -19,6 +24,8 @@ public class StudentTaskGUI {
         panel.setLayout(new GridLayout(0, 1));
 
         // Get tasks for student
+        ArrayList<Task> tasks = student.getTaskArrayList();
+        addButtons(tasks);
 
         // Add the panel to the frame and finish setup
         frame.add(panel, BorderLayout.CENTER);
@@ -26,5 +33,21 @@ public class StudentTaskGUI {
         frame.setTitle("Student Tasks for " + student.getName());
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void addButtons(ArrayList<Task> tasks) {
+        Iterator<Task> iterator = tasks.iterator();
+        while (iterator.hasNext()) {
+            Task task = iterator.next();
+            // Add Button for each task
+            JButtonStudentToCompleteTask specialButton = new JButtonStudentToCompleteTask(task.getName(), task);
+            panel.add(specialButton);
+            specialButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    JButtonStudentToCompleteTask tmpButton = (JButtonStudentToCompleteTask) event.getSource();
+                    tmpButton.execute();
+                }
+            });
+        }
     }
 }
