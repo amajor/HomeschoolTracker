1cf53509955bb5a552acaa7e97486872f48f3f75
Alison Major <alison.major@gmail.com>
2020-10-23 17:38:01 -0400

    Add javadocs for the user classes

--- src/main/java/homeschooltracker/users/Student.java
+++ src/main/java/homeschooltracker/users/Student.java
@@ -1,3 +1,6 @@
+/*
+ * (c) 2020 Alison Major. All rights reserved.
+ */
 package homeschooltracker.users;
 
 import homeschooltracker.content.Task;
@@ -5,35 +8,68 @@ import homeschooltracker.content.Task;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/**
+ * This is a class for the Student user.
+ * @author Alison Major
+ */
 public class Student {
-    String name;
-    ArrayList<Task> taskArrayList = new ArrayList<>();
+    private String name;
+    private ArrayList<Task> taskArrayList = new ArrayList<>();
 
+    /**
+     * The constructor for the {@code Student} class.
+     * @param name The name of the student
+     */
     public Student(String name)
     {
         this.name = name;
     }
 
+    /**
+     * Gets the student's name.
+     * @return The name of the student.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Adds a subject to the student.
+     * @see homeschooltracker.content.Subject
+     * @param subject
+     */
     public void add(Task subject) {
         taskArrayList.add(subject);
     }
 
+    /**
+     * Gets a subject from a specific position in the ArrayList.
+     * @param position The position in the {@code ArrayList} of a Student's subjects.
+     * @return The {@code Subject} from the position given.
+     */
     public Task getChild(int position) {
         return taskArrayList.get(position);
     }
 
+    /**
+     * Gets all of the subjects belonging to a {@code Student}.
+     * @see homeschooltracker.content.Subject
+     * @return An ArrayList of {@code Subject} tasks.
+     */
     public ArrayList<Task> getTaskArrayList() {
         return taskArrayList;
     }
 
+    /**
+     * Prints the name of the {@code Student}.
+     */
     public void print() {
         System.out.println("\nStudent: " + getName());
     }
 
+    /**
+     * Prints a list of tasks (subjects) for the {@code Student}.
+     */
     public void printTasks() {
         System.out.println("\n TODO LIST FOR " + getName());
         System.out.println("==========================================");
---
+++ src/main/java/homeschooltracker/users/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package holds the users, {@code Parent} and {@code Student}, that interact with the application.
+ */
+package homeschooltracker.users;
